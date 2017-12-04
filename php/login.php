d52cef410088558ed124faba76449b74e1c88f37
mram <mram@umd.edu>
2017-12-04 01:08:02 -0500

    Updated login

--- php/login.php
+++ php/login.php
@@ -1,51 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ExSidius
- * Date: 12/4/17
- * Time: 12:27 AM
- */
 
-    $login_nm = $_POST["uid"];
-    $login_passwd = $_POST["password"];
-
-    /* Establish a connection to the LDAP server */
-    $ldapconn=ldap_connect("ldap://ldap.umd.edu/",389) or die('Could not connect<br>');
-    // $ldapconn=ldap_connect("ldaps://ldap.umd.edu/",389) or die('Could not connect<br>');
-
-    /* Set the protocol version to 3 (unless set to 3 by default) */
-    ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-    /* Bind user to LDAP with password */
-    $verify_user=ldap_bind($ldapconn,"uid=$login_nm,ou=people,dc=umd,dc=edu",$login_passwd);
-
-    /* Returns 1 on Success */
-    if ($verify_user != 1) {
-        /* Failed */
-        echo "<h1>Invalid UID/password combination.</h1><br>";
-    } else {
-        /* Success */
-//        echo "You have been authenticated as having a valid UMD directory ID.";
-
-        $cookie_name = "uid";
-        $cookie_value = $login_nm;
-        $expire_time = time() + (86400 * 30);
-
-        setcookie($cookie_name, $cookie_value, $expire_time, "/");
+    if (isset($_COOKIE['uid'])) {
         header("Location: ../templates/student.html");
         exit();
     }
 
-    // Release connection
-    ldap_unbind($ldapconn);
-
 ?>
 
-<button type="button" onclick="returnToLogin()">Back to Login Page</button>
-<script>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scheduler Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+    <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Montserrat:400,700'>
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="../css/login.css">
 
-    function returnToLogin () {
-        window.location = "../Login.html";
-    }
+    <!-- CSS for bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
-</script>
+    <!-- JS for bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <!--CSS for Glyphicons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+
+<div class="form col-lg-5 col-md-6 col-sm-8 col-xs-12">
+
+    <!-- logo -->
+    <div id="logo">
+        <i class="material-icons" style="font-size:200px;color:#303b4f;text-shadow:2px 2px 6px #000000;">account_circle</i>
+    </div>
+    <!-- app name -->
+    <div class="container name">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 id="name">Office Hours Scheduler</h2>
+            </div>
+        </div>
+    </div>
+
+    <form action="./validate.php" method="post" class="login-form">
+        <input name="uid" type="text" placeholder="UID"/>
+        <input name="password" type="password" placeholder="password"/>
+        <button>login</button>
+    </form>
+
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="../js/index.js"></script>
+</body>
+</html>
