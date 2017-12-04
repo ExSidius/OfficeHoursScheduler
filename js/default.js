c9657f2c10504b4afcf5ef73bcebd36ad7669b5f
mram <mram@umd.edu>
2017-12-03 21:15:41 -0500

    Front end

---
+++ js/default.js
@@ -0,0 +1,8 @@
+$.getScript('//cdn.jsdelivr.net/isotope/1.5.25/jquery.isotope.min.js', function() {
+
+    /* activate jquery isotope */
+    $('.row').isotope({
+        itemSelector : '.item'
+    });
+
+});
